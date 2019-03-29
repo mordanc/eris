b182bd2ea8a2fe41581cc93cd2e8ee32d309cbac
Aaron Redman <aaron.redman.15@cnu.edu>
2019-03-29 15:18:16 -0400

    added main javasript and html pages

---
+++ main.js
@@ -0,0 +1,13 @@
+// style guidelines: tabs not spaces, single quotes not doubles, semicolons after each statement
+
+// The main.js should create windows and handle all the system events your application might encounter.
+
+const {app, BrowserWindow} = require('electron');
+
+function createWindow() {
+    let win = new BrowserWindow({width: 800, height: 800});
+
+    win.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
