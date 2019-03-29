fcf59d059c7e4f12ea2d4c2575528ad00f9cbea5
Aaron Redman <aaron.redman.15@cnu.edu>
2019-03-29 15:26:52 -0400

    Fixed file path errors caused by file structure

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const {app, BrowserWindow} = require('electron');
 function createWindow() {
     let win = new BrowserWindow({width: 800, height: 800});
 
-    win.loadFile('index.html');
+    win.loadFile('./src/index.html');
 }
 
 app.on('ready', createWindow);
